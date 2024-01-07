ad306f74c586ebd1256072f996ffc8edfa683763
Marcelle Santos <marcelledossantos2@gmail.com>
2024-01-06 22:38:48 -0300

    Add get controller

--- PotterTravel/Controllers/ClienteController.cs
+++ PotterTravel/Controllers/ClienteController.cs
@@ -1,12 +1,37 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PotterTravel.Data;
+using PotterTravel.Model;
 
 namespace PotterTravel.Controllers
 {
-    public class ClienteController
+    [ApiController]
+    [Route("api")]
+    public class ClienteController : ControllerBase
     {
-        
+        private readonly DataContext _dataContext;
+
+        public ClienteController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Cliente>>> GetClientes()
+        {
+            return await _dataContext.Clientes.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+
+        public async Task<ActionResult<Cliente>> GetClienteById(int id)
+        {
+            var cliente = await _dataContext.Clientes.FindAsync(id);
+            if (cliente == null)
+            {
+                return NotFound();
+            }
+            return Ok(cliente);
+        }
+
     }
 }
